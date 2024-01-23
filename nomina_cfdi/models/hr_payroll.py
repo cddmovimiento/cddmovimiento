e34473bd042f7fc675cf2532bda9c41c5727755d
KevinITGT <78069008+KevinITGT@users.noreply.github.com>
2024-01-23 17:25:59 -0600

    fix end date on build xml cfdi

--- nomina_cfdi/models/hr_payroll.py
+++ nomina_cfdi/models/hr_payroll.py
@@ -848,7 +848,7 @@ class HrPayslip(models.Model):
         payslip_total_JPRE = 0
 
         if self.contract_id.date_end:
-            antiguedad = (self.contract_id.date_end - self.contract_id.date_start + timedelta(days=1).days) / 7
+            antiguedad = ((self.contract_id.date_end - self.contract_id.date_start) + 1) / 7
         else:
             antiguedad = ((self.date_to - self.contract_id.date_start).days + 1) /7
 
