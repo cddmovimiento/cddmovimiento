233606371857a3aa5420c785f09899f70bcdb28a
Rodrigo Contreras <rodrigo@mrdc.tech>
2024-11-29 21:55:28 -0600

    Nomina CFDI bancos module rm

---
+++ nomina_cfdi_bancos/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Nomina CFDI bancos",
+    "author": "IT Admin",
+    "version": "17.0.1",
+    "category": "Other",
+    "description":"Genera dispersion de bancos para la nómina.",
+    "depends": ["nomina_cfdi"],
+    "data": [
+    ],
+    "license": 'AGPL-3',
+    'installable': True,
+}
