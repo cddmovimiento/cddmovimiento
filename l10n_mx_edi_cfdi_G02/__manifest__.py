64dc46e92e981c309c4a219f93ed9e911b7d1137
davozvm <dvalencia.ezzquad.com>
2026-02-10 15:27:46 -0600

    [REFACTOR]: actualizar nombre del módulo en el manifest

--- l10n_mx_edi_cfdi_G02/__manifest__.py
+++ l10n_mx_edi_cfdi_G02/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Nota de crédito uso CFDI G02',
+    'name': 'Descripción nota de crédito CFDI uso G02',
     'version': '1.0',
     'category': 'Accounting/Localizations/EDI',
     'summary': 'Ajuste para que los CFDI con uso G02 tengan la descripción correcta en los conceptos.',
